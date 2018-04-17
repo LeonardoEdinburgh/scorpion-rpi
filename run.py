50e5a80e599d49fa6d1b2b7b12795308b3ffe028
apg30 <apg30@users.noreply.github.com>
2018-04-17 16:56:46 +0100

    Experimental code. Added differential_Steering and cleaned up.

--- run.py
+++ run.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 import serial
 
-print("Setting up serial...")
-ser = serial.Serial('/dev/ttyACM0',115200)
-
-print("Reading...")
+direction_max = 100 
 previous_speed = 0
 previous_direction = 0
+left_motor = 0
+right_motor = 0
 
-def validate_serial(input, previous_speed, previous_direction):
+def init():
+	print("Setting up serial...")
+	ser = serial.Serial('/dev/ttyACM0',115200)
+	print("Reading...")
+
+# Validate the serial in then return the speed and direction.
+# If invalid return the previous values.
+def decoded_input(input, previous_speed, previous_direction):
     print("lenght:" + str(len(input)))
     if len(input) != 2:
         return previous_speed, previous_direction 
@@ -19,31 +25,36 @@ def validate_serial(input, previous_speed, previous_direction):
    # if (speed or direction > 100) or (speed or direction < 100):
    #     return previous_speed, previous_direction
 
-    # Store previous values
     previous_speed = speed
     previous_direction = direction
     return speed, direction
-
+	
+# Read serial input, decode it then return input.
 def read_serial():
-    # Read serial input
-    input = str(ser.readline())
-    # Split speed and direction values
+    input = str(ser.readline())     
     input = input.split('_')
-    # Validate serial input
-    valid_input = validate_serial(input, previous_speed, previous_direction)
-    print(valid_input)
-    return valid_input
+    decoded_input = decoded_input(input, previous_speed, previous_direction)
+    print(decoded_input)
+    return decoded_input
 
 def write_serial(input):
     ser.write(input)
     
-def differential_steering():
-    pass
-
+def differential_steering(speed, direction):
+    left_motor = speed
+	right_motor = speed
+	direction_percent = abs(direction) / direction_max
+	# If turn left
+	if direction < 0 : 
+		left_motor -= direction_percent * abs(speed)
+	# If turn right
+	elif direction > 0 : 
+		right_motor -= direction_percent * abs(speed)
 while 1:
 
+	init()
     speed, direction = read_serial()
-
+	differential_steering(speed, direction)
     print(speed)
     print(direction)
 
