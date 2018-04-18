eb9da409f6a49328e3198427d9a51651dae537ea
aidan <aidang123@googlemail.com>
2018-04-18 17:06:17 +0000

    cleaned up and added differential steering function

--- run.py
+++ run.py
@@ -2,60 +2,64 @@
 import serial
 
 direction_max = 100 
+direction_min = -100
+speed_max	 =  100
+speed_min	 = -100
+
 previous_speed = 0
 previous_direction = 0
-left_motor = 0
-right_motor = 0
 
-def init():
-	print("Setting up serial...")
-	ser = serial.Serial('/dev/ttyACM0',115200)
-	print("Reading...")
+print("Setting up serial...")
+ser = serial.Serial('/dev/ttyACM0',115200)
+print("Reading...")
 
 # Validate the serial in then return the speed and direction.
 # If invalid return the previous values.
-def decoded_input(input, previous_speed, previous_direction):
-    print("lenght:" + str(len(input)))
-    if len(input) != 2:
-        return previous_speed, previous_direction 
-    direction = input[0]
-    speed = input[1]
-    print("speed:" + speed)
-    print("dir:" + direction)
-   # if (speed or direction > 100) or (speed or direction < 100):
-   #     return previous_speed, previous_direction
-
-    previous_speed = speed
-    previous_direction = direction
-    return speed, direction
+def decode_input(input, previous_speed, previous_direction):
+	print("length:" + str(len(input)))
+	if len(input) == 2 : 
+		direction = input[0]
+		speed = input[1]
+		print("decoded direction:" + direction)
+		print("decoded speed:" + speed)
+		if (speed < speed_max and direction < direction_max) :
+			if( speed > speed_min and direction > direction_min) :
+				previous_speed = speed
+				previous_direction = direction
+				return speed, direction
+	# If not returned speed and direction yet then return previous
+	return previous_speed, previous_direction
 	
 # Read serial input, decode it then return input.
 def read_serial():
     input = str(ser.readline())     
     input = input.split('_')
-    decoded_input = decoded_input(input, previous_speed, previous_direction)
-    print(decoded_input)
+    decoded_input = decode_input(input, previous_speed, previous_direction)
+    print("decoded input: " + str(decoded_input[0] )+ str(decoded_input[1]))
     return decoded_input
 
 def write_serial(input):
     ser.write(input)
-    
+   
 def differential_steering(speed, direction):
-    left_motor = speed
+	left_motor = speed
 	right_motor = speed
-	direction_percent = abs(direction) / direction_max
+	direction_percent =  abs(direction) / float(direction_max)
+	print("dir perc " + str(direction_percent))
 	# If turn left
 	if direction < 0 : 
 		left_motor -= direction_percent * abs(speed)
 	# If turn right
 	elif direction > 0 : 
 		right_motor -= direction_percent * abs(speed)
-while 1:
+	print("left motor: " + str(left_motor))
+	print("right motor:  " + str(right_motor))
+	return left_motor, right_motor
 
-	init()
-    speed, direction = read_serial()
+while 1:
+	speed, direction = read_serial()
 	differential_steering(speed, direction)
-    print(speed)
-    print(direction)
+	print(speed)
+	print(direction)
 
 
